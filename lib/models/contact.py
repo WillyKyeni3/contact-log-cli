f409c56d548498e9bfff3d0ca0fd8f90cd898ad8
willymusyoka <willykyeni@gmail.com>
2025-08-26 02:18:52 +0300

    import base instead of session, Base

--- lib/models/contact.py
+++ lib/models/contact.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from lib.helpers import session, Base
+from lib.helpers import Base
 
 
 class Contact(Base):
