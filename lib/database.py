a46a58ed43a7ce40f76f1886ddf1950ed07d3de9
willymusyoka <willykyeni@gmail.com>
2025-08-26 21:27:10 +0300

    Replace connection_record with _ to tell SQLAlchemy to ignore this parameter

--- lib/database.py
+++ lib/database.py
@@ -8,7 +8,7 @@ import sqlite3
 
 # Enable foreign key constraints for SQLite
 @event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
+def set_sqlite_pragma(dbapi_connection, _):  # "_" tells SQLAlchemy to ignore this parameter "conneection_record"
     if isinstance(dbapi_connection, sqlite3.Connection):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON")
