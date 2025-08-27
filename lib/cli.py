f1e9cab9d5f4ade9fc9a349a8cc602b2e8d66bc9
willymusyoka <willykyeni@gmail.com>
2025-08-27 13:07:33 +0300

    Add menu options for viewing main menu and contact menu

--- lib/cli.py
+++ lib/cli.py
@@ -3,3 +3,22 @@ from lib.helpers import (
     create_communication, get_all_communications, find_communication_by_id,
     get_communications_for_contact
 )
+from lib.models import Contact, Communication
+
+# Display the menu options
+def menu():
+    """Main meni options display."""
+    print("\n===== CONTACT-LOG =====")
+    print("1. Manage Contacts")
+    print("2. Manage Communications")
+    print("0. Exit the application")
+    print("===========================")
+    
+def contact_menu():
+    """Display the contact management menu."""
+    print("\n===== CONTACT MANAGEMENT =====")
+    print("1. List all contacts")
+    print("2. Create a new contact")
+    print("3. View a contact's details")
+    print("4. Delete a contact")
+    print("0. Back to main menu")
