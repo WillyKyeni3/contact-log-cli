2383073ad192335cbb1c3896f687afd7e5dfbd7c
willymusyoka <willykyeni@gmail.com>
2025-08-27 12:56:43 +0300

    Import functions from helpers.py

---
+++ lib/cli.py
@@ -0,0 +1,5 @@
+from lib.helpers import (
+    create_contact, get_all_contact, find_contact_by_id,
+    create_communication, get_all_communications, find_communication_by_id,
+    get_communications_for_contact
+)
